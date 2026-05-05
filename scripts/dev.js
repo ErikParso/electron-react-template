1256add34229eb2cf7a4c01afefaa29324b5af61
Erik Parso <parsoerik@gmail.com>
2026-05-06 01:16:14 +0200

    removed unecesarz script

--- scripts/dev.js
+++
@@ -1,9 +0,0 @@
-const { spawn } = require('child_process');
-const path = require('path');
-
-const env = { ...process.env };
-delete env.ELECTRON_RUN_AS_NODE;
-
-const bin = path.join(__dirname, '../node_modules/.bin/electron-vite');
-const proc = spawn(bin, ['dev'], { env, stdio: 'inherit', shell: true });
-proc.on('close', (code) => process.exit(code ?? 0));
