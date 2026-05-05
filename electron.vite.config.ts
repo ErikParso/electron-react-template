77ced97c0d1d0751a3c5a4ac581ed5ec6f310b39
Erik Parso <parsoerik@gmail.com>
2026-05-06 01:25:29 +0200

    format

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -3,27 +3,27 @@ import react from '@vitejs/plugin-react';
 import swc from 'unplugin-swc';
 
 export default defineConfig({
-  main: {
-    plugins: [
-      externalizeDepsPlugin(),
-      swc.vite({ tsconfigFile: './tsconfig.node.json' }),
-    ],
-    build: {
-      rollupOptions: {
-        output: {
-          preserveModules: true,
-          preserveModulesRoot: 'src/main',
-        },
-      },
-    },
-  },
-  preload: {
-    plugins: [
-      externalizeDepsPlugin(),
-      swc.vite({ tsconfigFile: './tsconfig.node.json' }),
-    ],
-  },
-  renderer: {
-    plugins: [react()],
-  },
+	main: {
+		plugins: [
+			externalizeDepsPlugin(),
+			swc.vite({ tsconfigFile: './tsconfig.node.json' }),
+		],
+		build: {
+			rollupOptions: {
+				output: {
+					preserveModules: true,
+					preserveModulesRoot: 'src/main',
+				},
+			},
+		},
+	},
+	preload: {
+		plugins: [
+			externalizeDepsPlugin(),
+			swc.vite({ tsconfigFile: './tsconfig.node.json' }),
+		],
+	},
+	renderer: {
+		plugins: [react()],
+	},
 });
